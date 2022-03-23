c5edbd20c964b7374761c659091a5c8b8fb7ca1c
Z46X <agungprayogi078@gmail.com>
2022-03-23 21:50:53 +0700

    Project Taxi

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Membuat object Taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "AditPridavan";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();  
+        }
+    }
+}
